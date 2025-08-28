a24dd79fe472274d88c64eab30044dde53916391
soFurkan <132709978+soFurkan@users.noreply.github.com>
2025-08-28 09:35:38 +0300

    Sidebar Kısmı Düzenlendi

--- frontend/e-ticaret/apps/admin/src/pages/layouts/layouts.ts
+++ frontend/e-ticaret/apps/admin/src/pages/layouts/layouts.ts
@@ -1,11 +1,23 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, Signal, signal, ViewEncapsulation } from '@angular/core';
+import Breadcrumb from './breadcrumb';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { navigations } from '../../navigation';
+import { NavPipe } from '../../pipes/nav-pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [
+    Breadcrumb,
+    RouterLink,
+    RouterLinkActive,
+    NavPipe,
+    FormsModule],
   templateUrl: './layouts.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class Layouts {
+  readonly search = signal<string>("");
+  readonly navigations = computed(()=>navigations)
 
 }
