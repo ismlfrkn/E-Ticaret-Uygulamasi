bef791322ae51f37a6d3ac488431a96f1ffc560f
soFurkan <132709978+soFurkan@users.noreply.github.com>
2025-08-28 09:50:59 +0300

    Footer Yapısı Düzenlendi

--- frontend/e-ticaret/apps/admin/src/pages/layouts/layouts.ts
+++ frontend/e-ticaret/apps/admin/src/pages/layouts/layouts.ts
@@ -4,6 +4,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { navigations } from '../../navigation';
 import { NavPipe } from '../../pipes/nav-pipe';
 import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   imports: [
@@ -11,13 +12,23 @@ import { FormsModule } from '@angular/forms';
     RouterLink,
     RouterLinkActive,
     NavPipe,
-    FormsModule],
+    FormsModule,
+    DatePipe],
   templateUrl: './layouts.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class Layouts {
-  readonly search = signal<string>("");
-  readonly navigations = computed(()=>navigations)
+  readonly search = signal<string>(""); //Menü aramak için oluşturulmuş signal değişken(filtreleme yapılır)
+  readonly navigations = computed(()=>navigations) //NavigationModellerimizin tutulduğu computed.
+  readonly time = signal<Date | string>(""); //Footer'da güncel saat'in tutulduğu signal değişken
+
+  constructor()
+  {
+    setInterval(()=>
+    {
+      this.time.set(new Date());
+    },1000);
+  }
 
 }
