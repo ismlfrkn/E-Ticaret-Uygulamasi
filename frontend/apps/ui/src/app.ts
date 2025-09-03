695cd3bfe51f50d40c3f488a334d455d4b037087
ismlfrkn <bilenismailfurkan@gmail.com>
2025-09-03 12:11:21 +0300

    Admin Uygulamamız Tamamlandı UI uygulamamıza başlandı

---
+++ frontend/apps/ui/src/app.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  imports: [RouterModule],
+  selector: 'app-root',
+  template:`
+  <router-outlet></router-outlet>
+
+  
+  `
+  
+})
+export class App {
+ 
+}
